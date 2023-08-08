fd54b6520a57655e963235cfd5d70105921fc5aa
Jamil Guseynov <jamilgus@gmail.com>
2023-08-08 22:45:57 +0300

    update

--- source/main.d
+++ source/main.d
@@ -1,51 +1,47 @@
 import std.stdio;
 import bindbc.glfw;
+import core.thread.osthread;
+import core.time;
 
-void main()
+int main()
 {
-	/*
-	This version attempts to load the GLFW shared library using well-known variations
-	of the library name for the host system.
-	*/
 	GLFWSupport ret = loadGLFW();
 	if(ret != glfwSupport) {
 
-		// Handle error. For most use cases, its reasonable to use the the error handling API in
-		// bindbc-loader to retrieve error messages for logging and then abort. If necessary, it's
-		// possible to determine the root cause via the return value:
+		writeln("GLFW shared library failed to load");
 
 		if(ret == GLFWSupport.noLibrary) {
-			// GLFW shared library failed to load
+			writeln("Library not found");
 		}
 		else if(GLFWSupport.badLibrary) {
-			// One or more symbols failed to load. The likely cause is that the
-			// shared library is for a lower version than bindbc-glfw was configured
-			// to load (via GLFW_31, GLFW_32 etc.)
+			writeln("One or more symbols failed to load. Wrong version.");
 		}
+		return -1;
 	}
-	/*
-	This version attempts to load the GLFW library using a user-supplied file name.
-	Usually, the name and/or path used will be platform specific, as in this example
-	which attempts to load `glfw3.dll` from the `libs` subdirectory, relative
-	to the executable, only on Windows.
-	*/
-	// version(Windows) loadGLFW("libs/glfw3.dll")
 
 	if (!glfwInit())
 	{
-		// Initialization failed
+		writeln("GLFW initialization failed");
+		return -1;
 	}
 
-	GLFWwindow* window = glfwCreateWindow(640, 480, "My Title", null, null);
+	GLFWwindow* window = glfwCreateWindow(640, 480, "Infinity Space Shooter", null, null);
 	if (!window)
 	{
-		// Window or OpenGL context creation failed
+		writeln("Window or OpenGL context creation failed");
+		return -1;
 	}
+
+	static frameDuration = dur!("msecs")(17);
+
 	while (!glfwWindowShouldClose(window))
 	{
-		// Keep running
+		Thread.sleep(frameDuration);
+		glfwSwapBuffers(window);
+		glfwPollEvents();
 	}
 
 	glfwDestroyWindow(window);
 	glfwTerminate();
+	return 0;
 }
